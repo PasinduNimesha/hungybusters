14b7148d8d16d37531bda587189685785fd37bf0
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-04-08 12:06:30 +0530

    fixed overflow

---
+++ lib/components/FoodCard.dart
@@ -0,0 +1,57 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class FoodCard extends StatelessWidget {
+  final String name;
+  final String description;
+  final double price;
+  final double rating;
+  final VoidCallback onPressed;
+  const FoodCard({super.key, required this.name, required this.description, required this.price, required this.rating, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: const Icon(Icons.fastfood),
+        title: Title(name: name, rating: rating),
+        subtitle: Text(description),
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  final String name;
+  final double rating;
+  const Title({super.key, required this.name, required this.rating});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(name),
+            Icon(Icons.favorite),
+          ],
+        ),
+        //display the stars
+        Row(
+          children: [
+            for (var i = 0; i < 5; i++)
+              Icon(
+                i < rating ? Icons.star : Icons.star_border,
+                color: Colors.red,
+              ),
+            Text('$rating'),
+          ],
+        ),
+
+      ],
+    );
+  }
+}
+
--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hungybusters/components/FoodCard.dart';
 import 'package:hungybusters/components/OfferWindow.dart';
 
 import '../components/SideDrawer.dart';
@@ -18,8 +20,7 @@ class HomeScreen extends StatelessWidget {
       drawer: const SideDrawer(),
       body: Column(
         children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.08,
+          Container(
             child: const SearchBar(),
           ),
           Container(
@@ -36,16 +37,38 @@ class HomeScreen extends StatelessWidget {
               child: Text('Third Part'),
             ),
           ),
-          Container(
-            height: MediaQuery.of(context).size.height * 0.2,
-            color: Colors.yellow,
-            child: const Center(
-              child: Text('Fourth Part'),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  FoodCard(name: "Burger", price: 10.0, description: "This is a burger", rating: 3.5, onPressed: (){},),
+                  FoodCard(name: "Pizza", price: 15.0, description: "This is a pizza", rating: 4.5, onPressed: (){},),
+                  FoodCard(name: "Pasta", price: 12.0, description: "This is a pasta", rating: 4.0, onPressed: (){},),
+                  FoodCard(name: "Salad", price: 8.0, description: "This is a salad", rating: 3.0, onPressed: (){},),
+                ],
+              ),
             ),
           ),
         ],
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      )
     );
+
   }
 }
 
