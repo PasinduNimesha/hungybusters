7cd2cd5f856a25245800855c7818a30e87c9c3f5
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-04-10 11:21:00 +0530

    Create CartItemCard.dart

---
+++ lib/components/CartItemCard.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CartItemCard extends StatelessWidget {
+  const CartItemCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 5,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Row(
+        children: const [
+          Icon(
+            CupertinoIcons.delete,
+            color: Colors.red,
+          ),
+          Text('Burger'),
+          Text('10.0'),
+        ],
+      ),
+    );
+  }
+}
