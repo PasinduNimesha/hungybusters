055835b5afef2287ff8626101c090320b2ad4b25
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-04-10 09:11:51 +0530

    Update BottomNavBar.dart

--- lib/components/BottomNavBar.dart
+++ lib/components/BottomNavBar.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:hungybusters/screens/CheckoutScreen.dart';
+import 'package:hungybusters/screens/HomeScreen.dart';
 
-class BottomNavBar extends StatelessWidget {
+class BottomNavBar extends StatefulWidget {
   const BottomNavBar({
     super.key,
   });
 
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _currentIndex = 0;
+  String _title = 'Home';
+  final List<Widget> _children = [
+    const HomeScreen(),
+    const CheckoutScreen(),
+    const Placeholder(),
+  ];
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
@@ -14,8 +28,12 @@ class BottomNavBar extends StatelessWidget {
           label: 'Home',
         ),
         BottomNavigationBarItem(
+          icon: Icon(Icons.location_on_outlined),
+          label: 'Location',
+        ),
+        BottomNavigationBarItem(
           icon: Icon(Icons.shopping_cart),
-          label: 'Cart',
+          label: 'Checkout',
         ),
         BottomNavigationBarItem(
           icon: Icon(Icons.person),
