b4a64ddf750a244760cc23f737dc05e15be508f0
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-04-11 09:36:33 +0530

    Update PopCard.dart

--- lib/components/PopCard.dart
+++ lib/components/PopCard.dart
@@ -6,32 +6,33 @@ import 'package:hungybusters/components/PaymentMethodCard.dart';
 class PopUpCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(24),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          OrderSummary(),
-          SizedBox(height: 16),
-          TextField(
-            decoration: InputDecoration(
-              hintText: 'Apply Coupon Code',
-              border: OutlineInputBorder(),
+    return SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.all(24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            OrderSummary(),
+            SizedBox(height: 16),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Apply Coupon Code',
+                border: OutlineInputBorder(),
+              ),
             ),
-          ),
-          SizedBox(height: 40),
-          AddressCard(),
-          SizedBox(height: 16),
-          PaymentMethodCard(),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).pop(); // Close the pop-up card
-            },
-            child: Text('Close'),
-          ),
-          Expanded(child: Container()),
-        ],
+            SizedBox(height: 40),
+            AddressCard(),
+            SizedBox(height: 16),
+            PaymentMethodCard(),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the pop-up card
+              },
+              child: Text('Close'),
+            ),
+          ],
+        ),
       ),
     );
   }
