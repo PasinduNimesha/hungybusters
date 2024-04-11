8ac26020c87aee659c1b89daa670ffdf08e2eddc
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-04-11 09:14:40 +0530

    Update CheckoutScreen.dart

--- lib/screens/CheckoutScreen.dart
+++ lib/screens/CheckoutScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hungybusters/components/BottomNavBar.dart';
 import 'package:hungybusters/components/CartItemCard.dart';
+import 'package:hungybusters/components/MediumButton.dart';
 
 class CheckoutScreen extends StatelessWidget {
   const CheckoutScreen({super.key});
@@ -9,17 +10,32 @@ class CheckoutScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: const Column(
-          children: [
-            Text('Order Summary'),
-            CartItemCard(title: "Tex Mex BBQ", rating: 4.5, price: 100,),
-            CartItemCard(title: "Chicken Burger", rating: 4.0, price: 50,),
-            CartItemCard(title: "Chicken Wings", rating: 4.0, price: 50,),
-            CartItemCard(title: "Chicken Burger", rating: 4.0, price: 50,),
-            CartItemCard(title: "Chicken Wings", rating: 4.0, price: 50,),
-          ],
-        ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                        padding: const EdgeInsets.only(left: 18.0),
+                        alignment: Alignment.centerLeft,
+                        child: Text('Order Summary', style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),)
+                    ),
+                    CartItemCard(title: "Tex Mex BBQ", rating: 4.5, price: 100,),
+                    CartItemCard(title: "Chicken Burger", rating: 4.0, price: 50,),
+                    CartItemCard(title: "Chicken Wings", rating: 4.0, price: 50,),
+                    CartItemCard(title: "Chicken Burger", rating: 4.0, price: 50,),
+                    CartItemCard(title: "Chicken Wings", rating: 4.0, price: 50,),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          MediumButton(text: "Checkout", onPressed: () {}),
+        ],
       ),
     );
   }
